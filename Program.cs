11cdb18731f353bbba05ab171572349afca0a0d1
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-17 14:48:32 +0530

    bmi calculator

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BmiCalculatorApp.models;
+
+namespace BmiCalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] per = new Person[]
+            {
+                new Person(101, "Riya", 21),
+                new Person(201, "Roshani", 21, 1.75f, 55),
+                new Person(301, "Mary", 30, 2f, 100)
+            };
+
+            // Print information and body type for each person
+
+            foreach (Person person in per)
+            {
+                Console.WriteLine(person); //calls toString method
+                Console.WriteLine(Person.BodyType(person.CalculateBmiScore()));
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+}
---
+++ models/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculatorApp.models
+{
+    internal class Person
+    {
+
+        private double DEFAULT_WEIGHT = 50;
+        private float DEFAULT_HEIGHT = 1.524f;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public float Height { get; set; }
+
+        public double Weight { get; set; }
+
+
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Weight = DEFAULT_WEIGHT;
+            Height = (float)DEFAULT_HEIGHT;
+        }
+
+        public Person(int _id, string _name, int _age, float _height, double _weight) : this(_id, _name, _age)
+        {
+            Height = _height;
+            Weight = _weight;
+
+        }
+
+        public double CalculateBmiScore()
+        {
+            double bmiScore = (Weight) / (Height * Height);
+            return bmiScore;
+        }
+
+        public static string BodyType(double bmiScore)
+        {
+            if (bmiScore < 18.50)
+            {
+                return ("You are Underweight");
+            }
+            else if (bmiScore >= 18.5 && bmiScore <= 24.9)
+            {
+                return ("You are Healthy");
+            }
+            else if (bmiScore >= 25 && bmiScore <= 29.9)
+            {
+                return ("You are Overweight");
+            }
+            else
+            {
+                return ("You are Obese");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n" +
+                   $"Name: {Name}\n" +
+                   $"Age: {Age}\n" +
+                   $"BMI Score: {CalculateBmiScore():F2}\n";
+                
+        }
+
+
+
+
+
+    }
+}
